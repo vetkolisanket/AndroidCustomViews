7e4bfec3303d93f950402d28960e07eea577a1fc
Sanket Vetkoli <vetkolisanket@gmail.com>
2019-08-19 15:50:57 +0530

    Initial commit

---
+++ app/src/main/java/com/sanket/androidcustomviews/stack_image_view/StackImageActivity.kt
@@ -0,0 +1,13 @@
+package com.sanket.androidcustomviews.stack_image_view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.sanket.androidcustomviews.R
+
+class StackImageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stack_image)
+    }
+}
