89c5ef7d875837d1b4955cbae02cd3cdc70e7454
Sanket Vetkoli <vetkolisanket@gmail.com>
2019-09-21 20:21:54 +0530

    Opening CIA on click of its corresponding btn

--- app/src/main/java/com/sanket/androidcustomviews/MainActivity.kt
+++ app/src/main/java/com/sanket/androidcustomviews/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sanket.androidcustomviews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.sanket.androidcustomviews.circular_image_view.CircularImageActivity
 import com.sanket.androidcustomviews.stack_image_view.StackImageActivity
 import com.sanket.androidcustomviews.stack_text_view.StackTextActivity
 import com.sanket.androidcustomviews.timeline_view.TimelineViewActivity
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        btnCircularImage.setOnClickListener { startActivity(CircularImageActivity.newIntent(this)) }
         btnStackImageView.setOnClickListener { startActivity(StackImageActivity.newIntent(this)) }
         btnStackTextView.setOnClickListener { startActivity(StackTextActivity.newIntent(this)) }
         btnTimelineView.setOnClickListener { startActivity(TimelineViewActivity.newIntent(this)) }
--- app/src/main/java/com/sanket/androidcustomviews/circular_image_view/CircularImageActivity.kt
+++ app/src/main/java/com/sanket/androidcustomviews/circular_image_view/CircularImageActivity.kt
@@ -1,10 +1,16 @@
 package com.sanket.androidcustomviews.circular_image_view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.sanket.androidcustomviews.R
 
 class CircularImageActivity : AppCompatActivity() {
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, CircularImageActivity::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
