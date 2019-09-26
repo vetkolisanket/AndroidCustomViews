6b38a65bdd7ab729327aa8ebd6e91640c5fee215
Sanket Vetkoli <vetkolisanket@gmail.com>
2019-09-26 08:40:45 +0530

    Added empty badge view activity

--- app/src/main/java/com/sanket/androidcustomviews/MainActivity.kt
+++ app/src/main/java/com/sanket/androidcustomviews/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sanket.androidcustomviews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.sanket.androidcustomviews.badge_view.BadgeViewActivity
 import com.sanket.androidcustomviews.circular_image_view.CircularImageActivity
 import com.sanket.androidcustomviews.stack_image_view.StackImageActivity
 import com.sanket.androidcustomviews.stack_text_view.StackTextActivity
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         btnStackImageView.setOnClickListener { startActivity(StackImageActivity.newIntent(this)) }
         btnStackTextView.setOnClickListener { startActivity(StackTextActivity.newIntent(this)) }
         btnTimelineView.setOnClickListener { startActivity(TimelineViewActivity.newIntent(this)) }
+        btnBadgeView.setOnClickListener { startActivity(BadgeViewActivity.newIntent(this)) }
     }
 
 }
